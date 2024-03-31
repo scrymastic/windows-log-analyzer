2b02777ebd61939f4eb1eb9348aba83caba285f9
scrymastic <AT180142@actvn.edu.vn>
2024-03-31 12:34:51 +0700

    Added EventRecordID to log format, updated filter-engine

--- src/filter_engine.py
+++ src/filter_engine.py
@@ -1,18 +1,94 @@
-
-
-
+import regex
 
 class EngineFilter:
     def __init__(self):
         pass
 
-    def match(self, event, rule):
-        # Match the event with the rule and return True or False
-        # event: json object
-        # rule: yaml object
-        pass
+    def matches_rule(self, rule, event):
+        # Check if all conditions in the 'and' list are satisfied
+        if not self.matches_and_block(rule['and'], event):
+            return False
+        return True
+
+    def matches_and_block(self, block, event):
+        # Check if all conditions in the 'and' list are satisfied
+        for condition in block:
+            if not self.matches_condition(condition, event):
+                return False
+        return True
+
+    def matches_or_block(self, block, event):
+        # Check if any condition in the 'or' list is satisfied
+        for condition in block:
+            if self.matches_condition(condition, event):
+                return True
+        return False
+
+    def matches_condition(self, condition, event):
+        if not isinstance(condition, dict):
+            print(f"Invalid condition '{condition}'")
+            return False
+
+        # Handle 'and' or 'or' block
+        if len(condition) == 1:
+            key = next(iter(condition))
+            if key == 'and':
+                return self.matches_and_block(condition[key], event)
+            elif key == 'or':
+                return self.matches_or_block(condition[key], event)
+
+        # Extract the key and value from the condition
+        key, value = next(iter(condition.items()))
+        field, operator = key.split('|')
+
+        # Extract the field value from the event data
+        field_value = self.get_field_value(event, field)
+        if field_value is None:
+            return False
+        # Perform the comparison based on the operator
+        if operator == '==':
+            return field_value == value
+        elif operator == '!=':
+            return field_value != value
+        elif operator == 'contains':
+            return value in field_value
+        elif operator == 'not contains':
+            return value not in field_value
+        elif operator == 'startswith':
+            return field_value.startswith(value)
+        elif operator == 'endswith':
+            return field_value.endswith(value)
+        elif operator == 'matches':
+            return regex.match(value, field_value)
+        elif operator == 'not matches':
+            return not regex.match(value, field_value)
+        else:
+            print(f"Invalid operator '{operator}'")
+            return False
+
+
+    def get_field_value(self, event, field):
+        # Extract the field value from the event data
+        if field == 'EventID':
+            return event.get(field, None)
+        else:
+            event_data = event.get('EventData', {})
+            return event_data.get(field, None)
 
     def filter_events(self, events, rules):
         # Filter the events based on the rules
-        # Return the {event: rule id} dictionary
-        pass
+        # Return the {event id: rule id list} dictionary
+        filtered_events = {}
+        for event in events:
+            for rule_id, rule in rules.items():
+                if self.matches_rule(rule, event):
+                    if event['EventID'] in filtered_events:
+                        filtered_events[event['EventID']].append(rule_id)
+                    else:
+                        filtered_events[event['EventID']] = [rule_id]
+        return filtered_events
+    
+
+
+if __name__ == '__main__':
+    pass
--- tests/test_filter_engine.py
+++ tests/test_filter_engine.py
@@ -1,48 +1,93 @@
 
+import regex
 
-import yaml
+class EngineFilter:
+    def __init__(self):
+        pass
 
-# file_path = "D:\\AtSchool\\windows-log-analyzer\\rules\\legit-rules\\proc_creation_win_cmd_redirect.yml"
+    def matches_rule(self, rule, event):
+        # Check if all conditions in the 'and' list are satisfied
+        if not self.matches_and_block(rule['and'], event):
+            return False
+        return True
 
-# def read_rule(file_path):
-#     with open(file_path, 'r') as file:
-#         rule = yaml.safe_load(file)
-#         return rule
-    
-# rule = read_rule(file_path)
-# print(rule["detection"])
+    def matches_and_block(self, block, event):
+        # Check if all conditions in the 'and' list are satisfied
+        for condition in block:
+            if not self.matches_condition(condition, event):
+                return False
+        return True
 
-
-def matches_rule(rule, event):
-    # Check if all conditions in the 'and' list are satisfied
-    if not matches_and_condition(rule, event):
+    def matches_or_block(self, block, event):
+        # Check if any condition in the 'or' list is satisfied
+        for condition in block:
+            if self.matches_condition(condition, event):
+                return True
         return False
 
-    # If all 'and' conditions are satisfied, return True
-    return True
-
-def matches_and_condition(condition, event):
-    # Check if all conditions in the 'and' list are satisfied
-    for condition in condition.get('and', []):
-        if not matches_condition(condition, event):
+    def matches_condition(self, condition, event):
+        if not isinstance(condition, dict):
+            print(f"Invalid condition '{condition}'")
             return False
 
-    # If all 'and' conditions are satisfied, return True
-    return True
+        # Handle 'and' or 'or' block
+        if len(condition) == 1:
+            key = next(iter(condition))
+            if key == 'and':
+                return self.matches_and_block(condition[key], event)
+            elif key == 'or':
+                return self.matches_or_block(condition[key], event)
 
-def matches_or_condition(condition, event):
-    # Check if any condition in the 'or' list is satisfied
-    for condition in condition.get('or', []):
-        if matches_condition(condition, event):
-            return True
-        
-def matches_condition(condition, event):
-    # Extract the key and value from the condition
-    key, value = next(iter(condition.items()))
-    print(key, value)
+        # Extract the key and value from the condition
+        key, value = next(iter(condition.items()))
+        field, operator = key.split('|')
 
-    # If no 'or' condition is satisfied, return False
-    return False
+        # Extract the field value from the event data
+        field_value = self.get_field_value(event, field)
+        if field_value is None:
+            return False
+        # Perform the comparison based on the operator
+        if operator == '==':
+            return field_value == value
+        elif operator == '!=':
+            return field_value != value
+        elif operator == 'contains':
+            return value in field_value
+        elif operator == 'not contains':
+            return value not in field_value
+        elif operator == 'startswith':
+            return field_value.startswith(value)
+        elif operator == 'endswith':
+            return field_value.endswith(value)
+        elif operator == 'matches':
+            return regex.match(value, field_value)
+        elif operator == 'not matches':
+            return not regex.match(value, field_value)
+        else:
+            print(f"Invalid operator '{operator}'")
+            return False
+
+
+    def get_field_value(self, event, field):
+        # Extract the field value from the event data
+        if field == 'EventID':
+            return event.get(field, None)
+        else:
+            event_data = event.get('EventData', {})
+            return event_data.get(field, None)
+
+    def filter_events(self, events, rules):
+        # Filter the events based on the rules
+        # Return the {event id: rule id list} dictionary
+        filtered_events = {}
+        for event in events:
+            for rule_id, rule in rules.items():
+                if self.matches_rule(rule, event):
+                    if event['EventID'] in filtered_events:
+                        filtered_events[event['EventID']].append(rule_id)
+                    else:
+                        filtered_events[event['EventID']] = [rule_id]
+        return filtered_events
 
 
 # Example usage:
@@ -50,8 +95,8 @@ rule = {
     'and': [
         {'EventID|==': 1},
         {'or': [
-            {'OriginalFileName|==': 'Cmd.Exe'},
-            {'Image|endswith': '\\cmd.exe'}
+            {'Image|endswith': '\\cmd.exe'},
+            {'OriginalFileName|==': 'Cmd.Exe'}
             ]
         },
         {'CommandLine|contains': '>'},
@@ -76,10 +121,7 @@ event = {
     }
 }
 
-if matches_rule(rule, event):
-    print("Event matches the rule")
-else:
-    print("Event does not match the rule")
-
-
+filter_engine = EngineFilter()
+print(filter_engine.matches_rule(rule, event))  # Output: False
+# The event does not match the rule conditions
 
