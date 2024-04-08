8e2e6d883794d3318d4644ffbacae374d7cb3b03
scrymastic <AT180142@actvn.edu.vn>
2024-04-08 20:41:47 +0700

    add rule converter

--- src/rule_converter.py
+++ src/rule_converter.py
@@ -1,9 +1,175 @@
 
-
+import yaml
+import regex
+from pathlib import Path
+from config import ROOT
 
 class RuleConverter:
-    def __init__(self, rule):
-        self.rule = rule
+    def __init__(self):
+        self.legit_rules_folder = Path(ROOT, "rules", "legit-rules")
+        pass
 
-    def convert(self):
-        return self.rule
+
+    def convert(self, rule_path: str) -> bool:
+        rule_file = Path(rule_path)
+        if not rule_file.exists():
+            print(f"Rule file '{rule_file.name}' does not exist.")
+            return False
+        
+        with open(rule_file, 'r') as f:
+            try:
+                rule = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                print(f"Error loading YAML file: {e}")
+                return False
+            
+        # convert the rule['detection'] block
+        detection = rule.get('detection', {})
+        if detection:
+            try:
+                converted_detection = self.convert_detection_block(detection)
+            except ValueError as e:
+                print(e)
+                return False
+            converted_detection = [{'and': converted_detection}]
+            
+            # try to dump to file
+            converted_rule = rule.copy()
+            converted_rule['detection'] = converted_detection
+            converted_rule_path = Path(self.legit_rules_folder, rule_file.name)
+            with open(converted_rule_path, 'w') as f:
+                yaml.dump(converted_rule, f, sort_keys=False)
+                print(f"Rule '{converted_rule_path.name}' converted successfully.")
+                return True
+        else:
+            print(f"Rule '{rule_file.name}' has no detection block.")
+            return False
+
+    
+    def convert_detection_block(self, detection): 
+        conditions = detection.get('condition', '')
+        if not self.handle_condition(conditions):
+            raise ValueError(f'Unsupported condition: {conditions}')
+            
+        converted_detection = []
+        for key, value in detection.items():
+            if key == 'condition':
+                continue
+            if isinstance(value, dict):
+                converted_block = self.convert_and_block(value)
+            elif isinstance(value, list):
+                converted_block = self.convert_or_block(value)
+            else:
+                raise ValueError('Unexpected value type:', value)
+            
+            if 'selection' in key:
+                converted_detection.extend(converted_block)
+            elif 'filter_' in key:
+                converted_detection.extend(self.invert_conditions(converted_block))
+
+        return converted_detection
+    
+
+    def handle_condition(self, condition):
+        if regex.match(r'^(?:all of )?selection(?:_[^\s]*)?(?: (?:and not 1 of filter_[^\s]+))*', condition):
+            return True
+        else:
+            return False
+        
+
+    def convert_expression(self, expression):
+        mapping = {
+            'contains': 'contains',
+            'endswith': 'endswith',
+            'startswith': 'startswith',
+            're': 'matches',
+            'cidr': 'cidr',
+            'not contains': 'not contains',
+            'not endswith': 'not endswith',
+            'not startswith': 'not startswith',
+            'not re': 'not matches',
+            'not cidr': 'not cidr',
+            'contains|all': 'contains',
+        }
+        
+        if '|' in expression:
+            field, operator = expression.split('|', 1)
+            operator = mapping.get(operator)
+        else:
+            field = expression
+            operator = '=='
+
+        return f"{field}|{operator}"
+            
+
+    def convert_and_block(self, block):
+    # converted_block = {'and': []}
+        converted_block = []
+        for key, value in block.items():
+            if isinstance(value, list):
+                if key.endswith('|contains|all'):
+                    expression = self.convert_expression(key)
+                    sub_block = []
+                    for item in value:
+                        if isinstance(item, str):
+                            sub_block.append({expression: item})
+                        else:
+                            raise ValueError('Unexpected value type:', item)
+                    converted_block.extend(sub_block)           
+                else:
+                    expression = self.convert_expression(key)
+                    sub_block = {'or': []}
+                    for item in value:
+                        if isinstance(item, str):
+                            sub_block['or'].append({expression: item})
+                        elif isinstance(item, int):
+                            sub_block['or'].append({expression: item})
+                        else:
+                            raise ValueError('Unexpected value type:', item)
+                    converted_block.append(sub_block)
+
+            elif isinstance(value, str):
+                converted_block.append({self.convert_expression(key): value})
+            elif isinstance(value, int):
+                converted_block.append({self.convert_expression(key): value})
+            else:
+                raise ValueError('Unexpected value type:', value)
+
+        return converted_block
+
+
+    def convert_or_block(self, block):
+        converted_block = {'or': []}
+        for value in block:
+            if isinstance(value, dict):
+                converted_block['or'].extend(self.convert_and_block(value))
+            else:
+                raise ValueError('Unexpected value type:', value)
+        return [converted_block]
+    
+
+    def invert_conditions(self, conditions: list) -> list:
+        inverted_conditions = []
+        conditions = conditions[0].get('or', [])
+        if not conditions:
+            return inverted_conditions
+        for condition in conditions:
+            for key, value in condition.items():
+                field, operator = key.split('|', 1)
+                if operator == '==':
+                    operator = '!=='
+                elif operator == '!=':
+                    operator = '=='
+                else:
+                    if 'not' not in operator:
+                        operator = f'not {operator}'
+                    else:
+                        operator = operator.replace('not ', '')
+                inverted_conditions.append({f'{field}|{operator}': value})
+
+        return inverted_conditions
+
+if __name__ == '__main__':
+    rule_path = "D:\AtSchool\windows-log-analyzer\\rules\sigma-rules\process_creation\proc_creation_win_addinutil_uncommon_child_process.yml"
+    rule_converter = RuleConverter()
+    rule_converter.convert(rule_path)
--- tests/tmp.py
+++ tests/tmp.py
@@ -5,13 +5,15 @@ import json
 
 # log_file = "D:\AtSchool\windows-log-analyzer\sample-logs\CA_Mimikatz_Memssp_Default_Logs_Sysmon_11.evtx"
 
-parser = PyEvtxParser("D:\AtSchool\windows-log-analyzer\sample-logs\sideloading_wwlib_sysmon_7_1_11.evtx")
-events = []
+# parser = PyEvtxParser("D:\AtSchool\windows-log-analyzer\sample-logs\sideloading_wwlib_sysmon_7_1_11.evtx")
+# events = []
 
-for r in parser.records_json():
-    # print(r)
-    data = json.loads(r["data"]).get("Event", {})
-    event_id = data.get("System", {}).get("EventID", None)
-    print(event_id)
-    if event_id == 7:
-        print(data)
+# for r in parser.records_json():
+#     # print(r)
+#     data = json.loads(r["data"]).get("Event", {})
+#     event_id = data.get("System", {}).get("EventID", None)
+#     print(event_id)
+#     if event_id == 7:
+#         print(data)
+
+print("Hello World".split("|"))
